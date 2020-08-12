13daf62159a09935c2442d4f7571827a614b32cb
ivand <idalvarez@est.itsgg.edu.ec>
2020-08-12 16:58:47 -0500

    actualizacion 3

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project, Estudiantes
 
+admin.site.register(Project,Estudiantes)
 # Register your models here.
--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+class Project(models.Model):
+    title= models.CharField(max_length=200)
+    description= models.TextField()
+    user= models.CharField(max_length=100)
+    user_mod= models.CharField(max_length=100)
+    created= models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+class Estudiantes(models.Model):
+    nombre=models.CharField(max_length=100)
+    apellido= models.CharField(max_length=100)
+    edad= models.IntegerField
+    sexo=models.CharField(max_length=1)
+    email= models.EmailField
+    user= models.CharField(max_length=100)
+    user_mod= models.CharField(max_length=100)
+    created= models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name="estudiante"
+        verbose_name_plural="estudiantes"
+
+
 # Create your models here.
